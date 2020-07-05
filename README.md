6f37379d38249015f32c640a780e80b275119486
Selima Curci <s.curci@student.tue.nl>
2020-07-05 21:50:11 +0200

    Add model for comparison + monitoring of resources

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # Tutorial-SCADS-Summer-School-2020-Scalable-Deep-Learning
-Code associated with SCADS Summer School 2020 tutorial  "Scalable Deep Learning Tutorial": https://www.scads.de/de/summerschool2020
+Code associated with SCADS Summer School 2020 tutorial  "Scalable Deep Learning Tutorial".
+* Tutorial details - https://www.scads.de/de/summerschool2020
+* The code is based on [Implementation 2](https://github.com/dcmocanu/sparse-evolutionary-artificial-neural-networks/tree/master/SET-MLP-Sparse-Python-Data-Structures) of SET-MLP to which Dropout is added.
+* In the "Pretrained_results" folder there is a nice animation "fashion_mnist_connections_evolution_per_input_pixel_rand0.gif" of the input layer connectivity evolution during training.    
+* In the "optional_assignment" folder you can find the instructions for an additional hands-on experience.  For any question please feel free to contact me by email (s.curci@student.tue.nl). 
+
+
+######  Sparse Evolutionary Artificial Neural Networks
+* Proof of concept implementations of various sparse artificial neural network models with adaptive sparse connectivity trained with the Sparse Evolutionary Training (SET) procedure.  
+* The [SET implementations](https://github.com/dcmocanu/sparse-evolutionary-artificial-neural-networks)
+ are distributed in the hope that they may be useful, but without any warranties; Their use is entirely at the user's own risk.
+
+
+###### References
+
+For an easy understanding of these implementations please read the following articles. Also, if you use parts of this code in your work, please cite the corresponding ones:
+
+1. @article{Mocanu2018SET,
+  author =        {Mocanu, Decebal Constantin and Mocanu, Elena and Stone, Peter and Nguyen, Phuong H. and Gibescu, Madeleine and Liotta, Antonio},
+  journal =       {Nature Communications},
+  title =         {Scalable Training of Artificial Neural Networks with Adaptive Sparse Connectivity inspired by Network Science},
+  year =          {2018},
+  doi =           {10.1038/s41467-018-04316-3},
+  url =           {https://www.nature.com/articles/s41467-018-04316-3 }}
+
+2. @article{Mocanu2016XBM,
+author={Mocanu, Decebal Constantin and Mocanu, Elena and Nguyen, Phuong H. and Gibescu, Madeleine and Liotta, Antonio},
+title={A topological insight into restricted Boltzmann machines},
+journal={Machine Learning},
+year={2016},
+volume={104},
+number={2},
+pages={243--270},
+doi={10.1007/s10994-016-5570-z},
+url={https://doi.org/10.1007/s10994-016-5570-z }}
+
+3. @phdthesis{Mocanu2017PhDthesis,
+title = {Network computations in artificial intelligence},
+author = {Mocanu, Decebal Constantin},
+year = {2017},
+isbn = {978-90-386-4305-2},
+publisher = {Eindhoven University of Technology},
+url={https://pure.tue.nl/ws/files/69949254/20170629_CO_Mocanu.pdf }
+}
+
+4. @article{Liu2019onemillion,
+  author =        {Liu, Shiwei and Mocanu, Decebal Constantin and Mocanu and Ramapuram Matavalam, Amarsagar Reddy and Pei, Yulong Pei and Pechenizkiy, Mykola},
+  journal =       {arXiv:1901.09181},
+  title =         {Sparse evolutionary Deep Learning with over one million artificial neurons on commodity hardware},
+  year =          {2019},
+  url={https://arxiv.org/abs/1901.09181 }
+}
---
+++ dense_mlp.py
@@ -0,0 +1,352 @@
+# Authors: Decebal Constantin Mocanu et al.;
+# Code associated with SCADS Summer School 2020 tutorial "	Scalable Deep Learning Tutorial"; https://www.scads.de/de/summerschool2020
+# This is a pre-alpha free software and was tested in Windows 10 with Python 3.7.6, Numpy 1.17.2, SciPy 1.4.1, Numba 0.48.0
+
+# If you use parts of this code please cite the following article:
+# @article{Mocanu2018SET,
+#   author  =    {Mocanu, Decebal Constantin and Mocanu, Elena and Stone, Peter and Nguyen, Phuong H. and Gibescu, Madeleine and Liotta, Antonio},
+#   journal =    {Nature Communications},
+#   title   =    {Scalable Training of Artificial Neural Networks with Adaptive Sparse Connectivity inspired by Network Science},
+#   year    =    {2018},
+#   doi     =    {10.1038/s41467-018-04316-3}
+# }
+
+# If you have space please consider citing also these articles
+
+# @phdthesis{Mocanu2017PhDthesis,
+#   title     =    "Network computations in artificial intelligence",
+#   author    =    "D.C. Mocanu",
+#   year      =    "2017",
+#   isbn      =    "978-90-386-4305-2",
+#   publisher =    "Eindhoven University of Technology",
+# }
+
+# @article{Liu2019onemillion,
+#   author  =    {Liu, Shiwei and Mocanu, Decebal Constantin and Mocanu and Ramapuram Matavalam, Amarsagar Reddy and Pei, Yulong Pei and Pechenizkiy, Mykola},
+#   journal =    {arXiv:1901.09181},
+#   title   =    {Sparse evolutionary Deep Learning with over one million artificial neurons on commodity hardware},
+#   year    =    {2019},
+# }
+
+# We thank to:
+# Thomas Hagebols: for performing a thorough analyze on the performance of SciPy sparse matrix operations
+# Ritchie Vink (https://www.ritchievink.com): for making available on Github a nice Python implementation of fully connected MLPs. This SET-MLP implementation was built on top of his MLP code:
+#                                             https://github.com/ritchie46/vanilla-machine-learning/blob/master/vanilla_mlp.py
+
+from numba import njit
+from monitor import Monitor
+import datetime
+import json
+from nn_functions import *
+
+
+@njit(fastmath=True, cache=True)
+def compute_accuracy(activations, y_test):
+    correct_classification = 0
+    for j in range(y_test.shape[0]):
+        if np.argmax(activations[j]) == np.argmax(y_test[j]):
+            correct_classification += 1
+    return correct_classification / y_test.shape[0]
+
+
+@njit(fastmath=True, cache=True)
+def dropout(x, rate):
+    noise_shape = x.shape
+    noise = np.random.uniform(0., 1., noise_shape)
+    keep_prob = 1. - rate
+    scale = np.float32(1 / keep_prob)
+    keep_mask = noise >= rate
+    return x * scale * keep_mask, keep_mask
+
+
+class Dense_MLP:
+    def __init__(self, dimensions, activations):
+        """
+        :param dimensions: (tpl/ list) Dimensions of the neural net. (input, hidden layer, output)
+        :param activations: (tpl/ list) Activations functions.
+
+        Example of three hidden layer with
+        - 3312 input features
+        - 3000 hidden neurons
+        - 3000 hidden neurons
+        - 3000 hidden neurons
+        - 5 output classes
+
+
+        layers -->    [1,        2,     3,     4,     5]
+        ----------------------------------------
+
+        dimensions =  (3312,     3000,  3000,  3000,  5)
+        activations = (          Relu,  Relu,  Relu,  Sigmoid)
+        """
+        self.n_layers = len(dimensions)
+        self.loss = None
+        self.learning_rate = None
+        self.momentum = None
+        self.weight_decay = None
+        self.dropout_rate = 0.  # dropout rate
+        self.dimensions = dimensions
+
+        self.save_filename = ""
+        self.monitor = None
+
+        # Weights and biases are initiated by index. For a one hidden layer net you will have a w[1] and w[2]
+        self.w = {}
+        self.b = {}
+        self.pdw = {}
+        self.pdd = {}
+
+        # Activations are also initiated by index. For the example we will have activations[2] and activations[3]
+        self.activations = {}
+        for i in range(len(dimensions) - 1):
+            # He uniform initialization
+            limit = np.sqrt(6. / float(dimensions[i]))
+            self.w[i + 1] = np.random.uniform(-limit, limit, (dimensions[i], dimensions[i + 1]))
+            self.b[i + 1] = np.zeros(dimensions[i + 1])
+            self.activations[i + 2] = activations[i]
+
+    def _feed_forward(self, x, drop=False):
+        """
+        Execute a forward feed through the network.
+        :param x: (array) Batch of input data vectors.
+        :return: (tpl) Node outputs and activations per layer. The numbering of the output is equivalent to the layer numbers.
+        """
+        # w(x) + b
+        z = {}
+
+        # activations: f(z)
+        a = {1: x}  # First layer has no activations as input. The input x is the input.
+        masks = {}
+
+        for i in range(1, self.n_layers):
+            z[i + 1] = a[i] @ self.w[i] + self.b[i]
+            a[i + 1] = self.activations[i + 1].activation(z[i + 1])
+            if drop:
+                if i < self.n_layers - 1:
+                    # apply dropout
+                    a[i + 1], keep_mask = dropout(a[i + 1], self.dropout_rate)
+                    masks[i + 1] = keep_mask
+
+        return z, a, masks
+
+    def _back_prop(self, z, a, masks, y_true):
+        """
+        The input dicts keys represent the layers of the net.
+
+        a = { 1: x,
+              2: f(w1(x) + b1)
+              3: f(w2(a2) + b2)
+              4: f(w3(a3) + b3)
+              5: f(w4(a4) + b4)
+              }
+
+        :param z: (dict) w(x) + b
+        :param a: (dict) f(z)
+        :param y_true: (array) One hot encoded truth vector.
+        :return:
+        """
+        keep_prob = 1.
+        if self.dropout_rate > 0:
+            keep_prob = np.float32(1. - self.dropout_rate)
+
+        # Determine partial derivative and delta for the output layer.
+        # delta output layer
+        delta = self.loss.delta(y_true, a[self.n_layers])
+        dw = np.dot(a[self.n_layers - 1].T, delta)
+
+        update_params = {
+            self.n_layers - 1: (dw, np.mean(delta, axis=0))
+        }
+
+        # In case of three layer net will iterate over i = 2 and i = 1
+        # Determine partial derivative and delta for the rest of the layers.
+        # Each iteration requires the delta from the previous layer, propagating backwards.
+        for i in reversed(range(2, self.n_layers)):
+            # dropout for the backpropagation step
+            if keep_prob != 1:
+                delta = (delta @ self.w[i].transpose()) * self.activations[i].prime(z[i])
+                delta = delta * masks[i]
+                delta /= keep_prob
+            else:
+                delta = (delta @ self.w[i].transpose()) * self.activations[i].prime(z[i])
+
+            dw = np.dot(a[i - 1].T, delta)
+
+            update_params[i - 1] = (dw, np.mean(delta, axis=0))
+        for k, v in update_params.items():
+            self._update_w_b(k, v[0], v[1])
+
+    def _update_w_b(self, index, dw, delta):
+        """
+        Update weights and biases.
+
+        :param index: (int) Number of the layer
+        :param dw: (array) Partial derivatives
+        :param delta: (array) Delta error.
+        """
+
+        # perform the update with momentum
+        if index not in self.pdw:
+            self.pdw[index] = - self.learning_rate * dw
+            self.pdd[index] = - self.learning_rate * delta
+        else:
+            self.pdw[index] = self.momentum * self.pdw[index] - self.learning_rate * dw
+            self.pdd[index] = self.momentum * self.pdd[index] - self.learning_rate * delta
+
+        self.w[index] += self.pdw[index] - self.weight_decay * self.w[index]
+        self.b[index] += self.pdd[index] - self.weight_decay * self.b[index]
+
+    def fit(self, x, y_true, x_test, y_test, loss, epochs, batch_size, learning_rate=1e-3, momentum=0.9,
+            weight_decay=0.0002, dropoutrate=0., testing=True, save_filename="", monitor=False):
+        """
+        :param x: (array) Containing parameters
+        :param y_true: (array) Containing one hot encoded labels.
+        :return (array) A 2D array of metrics (epochs, 3).
+        """
+        if not x.shape[0] == y_true.shape[0]:
+            raise ValueError("Length of x and y arrays don't match")
+
+        self.monitor = Monitor(save_filename=save_filename) if monitor else None
+
+        # Initiate the loss object with the final activation function
+        self.loss = loss()
+        self.learning_rate = learning_rate
+        self.momentum = momentum
+        self.weight_decay = weight_decay
+        self.dropout_rate = dropoutrate
+        self.save_filename = save_filename
+
+        maximum_accuracy = 0
+
+        metrics = np.zeros((epochs, 4))
+
+        for i in range(epochs):
+            # Shuffle the data
+            seed = np.arange(x.shape[0])
+            np.random.shuffle(seed)
+            x_ = x[seed]
+            y_ = y_true[seed]
+
+            if self.monitor:
+                self.monitor.start_monitor()
+
+            # training
+            t1 = datetime.datetime.now()
+
+            for j in range(x.shape[0] // batch_size):
+                k = j * batch_size
+                l = (j + 1) * batch_size
+                z, a, masks = self._feed_forward(x_[k:l], True)
+
+                self._back_prop(z, a, masks, y_[k:l])
+
+            t2 = datetime.datetime.now()
+
+            if self.monitor:
+                self.monitor.stop_monitor()
+
+            print("\nDense-MLP Epoch ", i)
+            print("Training time: ", t2 - t1)
+
+            # test model performance on the test data at each epoch
+            # this part is useful to understand model performance and can be commented for production settings
+            if (testing):
+                t3 = datetime.datetime.now()
+                accuracy_test, activations_test = self.predict(x_test, y_test, batch_size)
+                accuracy_train, activations_train = self.predict(x, y_true, batch_size)
+                t4 = datetime.datetime.now()
+                maximum_accuracy = max(maximum_accuracy, accuracy_test)
+                loss_test = self.loss.loss(y_test, activations_test)
+                loss_train = self.loss.loss(y_true, activations_train)
+                metrics[i, 0] = loss_train
+                metrics[i, 1] = loss_test
+                metrics[i, 2] = accuracy_train
+                metrics[i, 3] = accuracy_test
+
+                print(f"Testing time: {t4 - t3}\n; Loss test: {loss_test}; \n"
+                      f"Accuracy test: {accuracy_test}; \n"
+                      f"Maximum accuracy val: {maximum_accuracy}")
+
+            # save performance metrics values in a file
+            if save_filename != "":
+                np.savetxt(save_filename, metrics)
+
+            if self.save_filename != "" and self.monitor:
+                with open(self.save_filename + "_monitor.json", 'w') as file:
+                    file.write(json.dumps(self.monitor.get_stats(), indent=4, sort_keys=True, default=str))
+
+        return metrics
+
+    def predict(self, x_test, y_test, batch_size=100):
+        """
+        :param x_test: (array) Test input
+        :param y_test: (array) Correct test output
+        :param batch_size:
+        :return: (flt) Classification accuracy
+        :return: (array) A 2D array of shape (n_cases, n_classes).
+        """
+        activations = np.zeros((y_test.shape[0], y_test.shape[1]))
+        for j in range(x_test.shape[0] // batch_size):
+            k = j * batch_size
+            l = (j + 1) * batch_size
+            _, a_test, _ = self._feed_forward(x_test[k:l], drop=False)
+            activations[k:l] = a_test[self.n_layers]
+        accuracy = compute_accuracy(activations, y_test)
+        return accuracy, activations
+
+
+def load_fashion_mnist_data(no_training_samples, no_testing_samples):
+    np.random.seed(0)
+
+    data = np.load("data/fashion_mnist.npz")
+
+    index_train = np.arange(data["X_train"].shape[0])
+    np.random.shuffle(index_train)
+
+    index_test = np.arange(data["X_test"].shape[0])
+    np.random.shuffle(index_test)
+
+    x_train = data["X_train"][index_train[0:no_training_samples], :]
+    y_train = data["Y_train"][index_train[0:no_training_samples], :]
+    x_test = data["X_test"][index_test[0:no_testing_samples], :]
+    y_test = data["Y_test"][index_test[0:no_testing_samples], :]
+
+    # normalize in 0..1
+    x_train = x_train.astype('float64') / 255.
+    x_test = x_test.astype('float64') / 255.
+
+    return x_train, y_train, x_test, y_test
+
+
+if __name__ == "__main__":
+
+    for i in range(5):
+        # load data
+        no_training_samples = 5000  # max 60000 for Fashion MNIST
+        no_testing_samples = 1000  # max 10000 for Fshion MNIST
+        x_train, y_train, x_test, y_test = load_fashion_mnist_data(no_training_samples, no_testing_samples)
+
+        #set model parameters
+        no_hidden_neurons_layer = 1000
+        no_training_epochs = 400
+        batch_size = 40
+        dropout_rate = 0.2
+        learning_rate = 0.001
+        momentum = 0.9
+        weight_decay = 0.0002
+
+        np.random.seed(i)
+
+        # create Dense-MLP ( fully-connected MLP)
+        dense_mlp = Dense_MLP((x_train.shape[1], no_hidden_neurons_layer, no_hidden_neurons_layer, no_hidden_neurons_layer, y_train.shape[1]),
+                              (Relu, Relu, Relu, Softmax))
+
+        # train Dense-MLP
+        dense_mlp.fit(x_train, y_train, x_test, y_test, loss=CrossEntropy, epochs=no_training_epochs, batch_size=batch_size, learning_rate=learning_rate,
+                      momentum=momentum, weight_decay=weight_decay, dropoutrate=dropout_rate, testing=True,
+                      save_filename="Pretrained_results/dense_mlp_"+str(no_training_samples)+"_training_samples_rand"+str(i), monitor=True)
+
+        # test Dense-MLP
+        accuracy, _ = dense_mlp.predict(x_test, y_test, batch_size=1)
+
+        print("\nAccuracy of the last epoch on the testing data: ", accuracy)
